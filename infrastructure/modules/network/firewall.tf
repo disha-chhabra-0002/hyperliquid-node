9569acef41f8b9c190473f28109aab6f57ce1758
disha-chhabra-0002 <dchhabra@deqode.com>
2026-01-12 15:40:24 +0530

    edited firewall

--- infrastructure/modules/network/firewall.tf
+++ infrastructure/modules/network/firewall.tf
@@ -1,16 +1,11 @@
 resource "google_compute_firewall" "general" {
   name    = "${var.vpc_name}-hyperliquid-node"
-  
+
   network = google_compute_network.vpc.name
 
   allow {
     protocol = "tcp"
-    ports    = ["4001" , "4002" , "3001", "22"]
-  }
-
-  allow {
-    protocol = "http"
-    ports    = ["9100"]
+    ports    = ["4001" , "4002" , "3001", "22" ,  "9100"]
   }
 
 
